9c7a56a7bf943b3aab693482a9aaee61251e42fe
John Clow <jclow@fb.com>
2021-12-17 16:13:03 -0800

    Adding custom device type change rules
    ghstack-source-id: 141a4ac1d5458bf987a043d18d6af6f579800870
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69051

--- torch/csrc/jit/passes/device_type_analysis.cpp
+++ torch/csrc/jit/passes/device_type_analysis.cpp
@@ -47,6 +47,11 @@ bool setReturnsToDevice(Node* n, c10::optional<Device> device) {
   return changed;
 }
 
+PropRule setReturnstoDeviceRule(DeviceType deviceType) {
+  Device device = Device(deviceType);
+  return [=](Node* n) { return setReturnsToDevice(n, device); };
+}
+
 bool propWithNoDevice(Node* n) {
   // Figure out what the common device to propagate is
   // Types of tensors must match, except CPU zerodim, which any
@@ -121,6 +126,7 @@ bool defaultDeviceProp(Node* n) {
 struct DeviceTypePropagationPass {
   explicit DeviceTypePropagationPass(std::shared_ptr<Graph> graph)
       : graph_(std::move(graph)) {
+    buildRuleRegistry();
   }
 
   // returns true if at least one node has its scalar type set on a tensor node
@@ -179,7 +185,6 @@ struct DeviceTypePropagationPass {
       const at::ArrayRef<Value*>& src1,
       const at::ArrayRef<Value*>& src2,
       const at::ArrayRef<Value*>& dst) {
-
     TORCH_INTERNAL_ASSERT(src1.size() == src2.size());
     TORCH_INTERNAL_ASSERT(src1.size() == dst.size());
 
@@ -231,10 +236,30 @@ struct DeviceTypePropagationPass {
     if (!op) {
       return;
     }
+    auto prop_fn = device_prop_registry_->find(*op);
+    if (prop_fn) {
+      PropRule rule = *prop_fn;
+      changed_ |= rule(n);
+      return;
+    }
     changed_ |= defaultDeviceProp(n);
   }
 
+  void buildRuleRegistry() {
+    // building a registry for all of the custom Device Type rules
+    static const OperatorMap<PropRule> temp_registry{
+        {"aten::cpu(Tensor self) -> Tensor",
+         setReturnstoDeviceRule(DeviceType::CPU)},
+        {"aten::cuda(Tensor self) -> Tensor",
+         setReturnstoDeviceRule(DeviceType::CUDA)},
+        {"aten::to_mkldnn(Tensor self, ScalarType? dtype) -> Tensor",
+         setReturnstoDeviceRule(DeviceType::MKLDNN)},
+    };
+    device_prop_registry_ =
+        std::make_unique<OperatorMap<PropRule>>(temp_registry);
+  }
 
+  std::unique_ptr<OperatorMap<PropRule>> device_prop_registry_;
   std::shared_ptr<Graph> graph_;
   bool changed_ = false;
 };
